7974b5f1e2e30e9301b82700152429bc39de8b42
stagjava <stagjava@stag-X756UQ>
2020-06-08 10:56:23 +0200

    Premier commit

---
+++ pronotest/src/java/fr/agy/control/Spays.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.agy.control;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author stagjava
+ */
+public class Spays extends HttpServlet {
+    
+}
